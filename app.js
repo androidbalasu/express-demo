2ec6a22c3b80c65891a6c43b3ebc59b5e1ff4615
Prashanth.Anantharam <Prashanth.Anantharam@bentley.com>
2018-07-09 21:58:13 -0500

    Add another get\post route to be able to send a list of hardcoded courses.

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+app.get('/api/courses', (req, res)=>{
+    console.log('Sending back courses list');
+    res.send([1, 2, 3]);
+});
+
 app.listen('3000', () => {
     console.log('Listening on port 3000....');
 });
