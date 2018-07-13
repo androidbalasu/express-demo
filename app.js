48f771bd401b506d872e936cb4d860a73616f7c8
Prashanth.Anantharam <Prashanth.Anantharam@bentley.com>
2018-07-12 21:28:16 -0500

    Get request with query parameters

--- app.js
+++ app.js
@@ -5,15 +5,38 @@ const express = require('express');
 //Returns an object of type Express
 const app = express();
 
+const courses = [
+    {id: 1, name: 'course1'},
+    {id: 2, name: 'course2'},
+    {id: 3, name: 'course3'},
+    {id: 4, name: 'course4'},
+];
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+//Endpoint to return all courses
 app.get('/api/courses', (req, res)=>{
     console.log('Sending back courses list');
     res.send([1, 2, 3]);
 });
 
-app.listen('3000', () => {
-    console.log('Listening on port 3000....');
+//Endpoint to return the request course
+// /api/courses/1
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id);
+});
+
+// app.get('/api/posts/:month/:year', (req, res) => {
+//     res.send(req.params);
+// });
+
+app.get('/api/posts/:month/:year', (req, res) => {
+    res.send(req.query);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}....`);
 });
